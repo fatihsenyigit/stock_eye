74de45e6fe85b689d23e51e2cbc2534c701ec838
fatih <fsenyigit8@gmail.com>
2024-06-20 16:18:22 +0300

    menu list

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,31 +1,184 @@
-import React from "react"
-import AppBar from "@mui/material/AppBar"
-import Box from "@mui/material/Box"
-import CssBaseline from "@mui/material/CssBaseline"
+// import React from "react"
+// import AppBar from "@mui/material/AppBar"
+// import Box from "@mui/material/Box"
+// import CssBaseline from "@mui/material/CssBaseline"
 
-import Toolbar from "@mui/material/Toolbar"
+// import Toolbar from "@mui/material/Toolbar"
+// import Button from "@mui/material/Button"
+// import Typography from "@mui/material/Typography"
+// import { useSelector } from "react-redux"
+// import useApiRequest from "../services/useApiRequest"
+
+// function Dashboard() {
+//   const {user} = useSelector((state) => state.auth)
+//   const {logout} = useApiRequest()
+
+//   return (
+//     <Box sx={{ display: "flex" }}>
+//       <CssBaseline />
+//       <AppBar position="fixed">
+//         <Toolbar>
+//           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+//             STOCK APP
+//           </Typography>
+//           {user && (<Button color="inherit" onClick={logout}>Logout</Button>)}
+//         </Toolbar>
+//       </AppBar>
+//     </Box>
+//   )
+// }
+
+// export default Dashboard
+
+
+
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+// import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button"
-import Typography from "@mui/material/Typography"
 import { useSelector } from "react-redux"
 import useApiRequest from "../services/useApiRequest"
+import MenuList from "../components/MenuList";
 
-function Dashboard() {
-  const {user} = useSelector((state) => state.auth)
-  const {logout} = useApiRequest()
+const drawerWidth = 200;
+
+function Dashboard(props) {
+     const {user} = useSelector((state) => state.auth)
+     const {logout} = useApiRequest()
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [isClosing, setIsClosing] = React.useState(false);
+
+  const handleDrawerClose = () => {
+    setIsClosing(true);
+    setMobileOpen(false);
+  };
+
+  const handleDrawerTransitionEnd = () => {
+    setIsClosing(false);
+  };
+
+  const handleDrawerToggle = () => {
+    if (!isClosing) {
+      setMobileOpen(!mobileOpen);
+    }
+  };
+
+  const drawer = (
+    <div>
+      <Toolbar />
+      
+        <MenuList></MenuList>
+      
+      
+    </div>
+  );
+
+  // Remove this const when copying and pasting into your project.
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar position="fixed">
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            STOCK APP
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div" sx={{flexGrow:1}}>
+            Stock Eye
           </Typography>
-          {user && (<Button color="inherit" onClick={logout}>Logout</Button>)}
+
+          {user && (
+            <Button color="inherit" onClick={logout}>
+              Logout
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onTransitionEnd={handleDrawerTransitionEnd}
+          onClose={handleDrawerClose}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
+        <Toolbar />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+          dolor purus non enim praesent elementum facilisis leo vel. Risus at
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
+          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
+        </Typography>
+      </Box>
     </Box>
-  )
+  );
 }
 
-export default Dashboard
+
+export default Dashboard;
---
+++ src/pages/Firms.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const Firms = () => {
+  return (
+    <div>Firms</div>
+  )
+}
+
+export default Firms
