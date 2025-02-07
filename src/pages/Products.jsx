1a7f356109928abd01b7e987daae13fa71c49392
fatihsenyigit <fsenyigit8@gmail.com>
2025-02-07 22:00:41 +0700

    product page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,10 +1,59 @@
+import React, { useEffect, useState } from "react";
+import useStockRequest from "../services/useStockRequest";
+import { useSelector } from "react-redux";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import ProductTable from "../components/ProductTable";
+import ProductModal from "../components/ProductModal";
 
-import React from 'react'
+const Firms = () => {
+  const { getStock } = useStockRequest();
+  const { firms } = useSelector((state) => state.stock);
 
-const Products = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+
+  const [info, setInfo] = React.useState({
+    name: "",
+    phone: "",
+    image: "",
+    address: "",
+  });
+
+  const handleClose = () => {
+    setOpen(false);
+    setInfo({
+      name: "",
+      phone: "",
+      image: "",
+      address: "",
+    });
+  };
+
+  useEffect(() => {
+    getStock("products");
+  }, []);
+
   return (
-    <div>Products</div>
-  )
-}
+    <div>
+      <Typography variant="h4" color={"error"} mb={2}>
+        Products
+      </Typography>
 
-export default Products
+      <Button variant="contained" onClick={handleOpen} sx={{mb:3}}>
+        New Product
+      </Button>
+
+      <ProductModal
+        open={open}
+        handleClose={handleClose}
+        info={info}
+        setInfo={setInfo}
+      ></ProductModal>
+
+      <ProductTable></ProductTable>
+    </div>
+  );
+};
+
+export default Firms;
