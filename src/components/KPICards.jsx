25bdde63d4975d9e548718f3d069d8bc0ac829b9
fatihsenyigit <fsenyigit8@gmail.com>
2025-02-14 19:36:45 +0700

    kpi data

--- src/components/KPICards.jsx
+++ src/components/KPICards.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import Paper from "@mui/material/Paper";
+import EuroIcon from "@mui/icons-material/Euro";
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { Stack } from "@mui/material";
 
 const KPICards = () => {
+  const kpiData = [
+    {
+      id: 1,
+      title: "Sales",
+      icon: <EuroIcon />,
+      amount: "€12000",
+      color: "purple",
+      bgColor: "pink",
+    },
+    {
+        id: 2,
+        title: "Profit",
+        icon: <ShoppingBasketIcon/>,
+        amount: "€16000",
+        color: "purple",
+        bgColor: "pink",
+      },
+  ];
   return (
-    <div>
-      <Paper elevation={3}></Paper>
-    </div>
+    <Stack justifyContent={"center"} alignItems={"center"} flexWrap={'wrap'}>
+        {kpiData.map((item)=> (
+            <Paper key={item.id} elevation={3}></Paper>
+        ))}
+      
+    </Stack>
   );
 };
 
